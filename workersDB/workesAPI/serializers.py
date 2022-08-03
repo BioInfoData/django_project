8be84433526abe369545f66638c2a71b197b7e4e
Reut Fluss <reut.ashwalfluss@gmail.com>
2022-08-03 22:41:31 +0300

    change api

--- workersDB/workesAPI/serializers.py
+++ workersDB/workesAPI/serializers.py
@@ -4,7 +4,7 @@ from workers.models import Worker, Department
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
-        fields = [ 'id', 'department_id', 'description', 'thumb', 'slug'
+        fields = [ 'id', 'department_id', 'description'
                  ]
 
 
@@ -12,5 +12,5 @@ class WorkerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Worker
         fields = [ 'id', 'employee_id', 'first_name','last_name','department_id', 'job_id',
-                   'salary','manager_id','commission_pct','email','phone_number','heir_date', 'thumb', 'slug'
+                   'salary','manager_id','commission_pct','email','phone_number','heir_date'
                  ]
